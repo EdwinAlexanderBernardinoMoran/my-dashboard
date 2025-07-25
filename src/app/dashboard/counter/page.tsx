625cb8869c58f17a509d9a27fb5dfb329c828d5f
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-07-25 11:05:15 -0600

    S4 - Pensemos en hojas y pequenos componentes (Agregando libreria de iconos)

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,19 +1,17 @@
-"use client";
+import { CartCounter } from "@/app/shopping-cart";
 
-import { useState } from "react";
+export const metadata = {
+  title: 'Shpopping Cart',
+  description: 'Counter page for the shopping cart',
+};
 
 export default function CounterPage() {
 
-  const [counter, setCounter] = useState(0)
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el carrito</span>
-      <span className="text-9xl font-bold">{counter}</span>
 
-      <div className="flex">
-        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2" onClick={ () => setCounter(counter + 1)}>+1</button>
-        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2" onClick={ () => setCounter(counter > 0 ? counter - 1 : 0)}>-1</button>
-      </div>
+      <CartCounter value={5} />
     </div>
   );
 }
